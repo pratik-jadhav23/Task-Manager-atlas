d9ef804a665adc37e43c4f7e3f087137d7c8b137
broy-vec <vector.indiafs@gmail.com>
2025-12-06 17:16:56 +0530

    changes in index.js

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ const rt = require("./routes/routes");
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO_URI, {
+  .connect(process.env.MONGO_URI, { 
     // useNewUrlParser: true,
     // useUnifiedTopology: true,
   })
